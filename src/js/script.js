9fbea6d7cb166ae0d5fc9f34ccb8bb6e1151485b
Michał <bodysmic@gmail.com>
2022-10-17 17:11:46 +0200

    acordeon improve

--- src/js/script.js
+++ src/js/script.js
@@ -84,12 +84,9 @@
       /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
-        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        for(let activeProduct of activeProducts){
-          (activeProduct !== null && activeProduct !== thisProduct.element) ? activeProduct.classList.remove  ('active') : thisProduct.element;
-          console.log('removed active:', activeProduct);
-        }
+        if(activeProduct !== null && activeProduct !== thisProduct.element) activeProduct.classList.remove('active');
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
         console.log('active toggle:', thisProduct.element);
