448c0647f898e63025cfffbef88e0014befa271b
Michał <bodysmic@gmail.com>
2022-10-17 16:51:24 +0200

    acordeon fuction

--- dist/js/script.js
+++ dist/js/script.js
@@ -58,7 +58,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
-
+      thisProduct.initAccordion();
       console.log(('new Product:', thisProduct));
     }
 
@@ -74,6 +74,26 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       // add element to menu
       menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+      const thisProduct = this;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        for(let activeProduct of activeProducts){
+          (activeProduct !== null && activeProduct !== thisProduct.element) ? activeProduct.classList.remove  ('active') : thisProduct.element;
+          console.log('removed active:', activeProduct);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+        console.log('active toggle:', thisProduct.element);
+      });
     }
   }
 
